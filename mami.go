fd9feab375ecbcc6994ea5c87858f7f268a8c92d
sanemat <o.gata.ken@gmail.com>
2016-01-04 00:20:48 +0900

    feat(mami): use MAMI_DIR

--- mami.go
+++ mami.go
@@ -1,20 +1,34 @@
 package main
 
 import (
+	"errors"
 	"flag"
 	"fmt"
 	"log"
+	"os"
 	"time"
 )
 
 // memo: basename = time.strftime('%Y-%m-%d-%H-%M-%S')
 const timeFormat = "2006-01-02-15-04-05"
 
+// ErrDirectory is the error resulting if no directory given
+var ErrDirectory = errors.New("mami requires MAMI_DIR or directory option")
+
 var givenDir = flag.String("d", "", "target directory")
 
 func mami(current time.Time, givenDir string) (string, error) {
 	baseName := current.Format(timeFormat)
-	return givenDir + baseName + ".txt", nil
+	var targetDir string
+	if givenDir != "" {
+		targetDir = givenDir
+	} else {
+		targetDir = os.Getenv("MAMI_DIR")
+	}
+	if targetDir == "" {
+		return "", ErrDirectory
+	}
+	return targetDir + baseName + ".txt", nil
 }
 
 func main() {
