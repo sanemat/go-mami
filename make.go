eccdf012732579a327183f76f3abb61fd98a6901
sanemat <o.gata.ken@gmail.com>
2016-01-03 23:02:23 +0900

    chore(make): add build scripts

---
+++ make.go
@@ -0,0 +1,119 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"runtime"
+)
+
+func buildCommand(fileName string, commandName string) (string, error) {
+	var commandNameWithExt string
+	if runtime.GOOS == "windows" {
+		commandNameWithExt = commandName + ".exe"
+	} else {
+		commandNameWithExt = commandName
+	}
+	builtFile := filepath.Join("out", commandNameWithExt)
+	comm := []string{"go", "build", "-o", builtFile, fileName}
+	if _, err := exec.Command(comm[0], comm[1:]...).Output(); err != nil {
+		return builtFile, err
+	}
+	return builtFile, nil
+}
+
+func buildMami() {
+	if out, err := buildCommand("mami.go", "mami"); err != nil {
+		log.Fatal(err)
+	} else {
+		fmt.Println(out)
+	}
+}
+
+func fmtAll() {
+	comm := []string{"go", "fmt", "./..."}
+	if out, err := exec.Command(comm[0], comm[1:]...).Output(); err != nil {
+		log.Fatal(err)
+	} else {
+		fmt.Print(string(out))
+	}
+}
+
+func beforeScript() {
+	var comm []string
+	comm = []string{"go", "get", "golang.org/x/tools/cmd/vet"}
+	if out, err := exec.Command(comm[0], comm[1:]...).Output(); err != nil {
+		log.Fatal(err)
+	} else {
+		fmt.Print(string(out))
+	}
+	comm = []string{"go", "get", "golang.org/x/tools/cmd/goimports"}
+	if out, err := exec.Command(comm[0], comm[1:]...).Output(); err != nil {
+		log.Fatal(err)
+	} else {
+		fmt.Print(string(out))
+	}
+	comm = []string{"go", "get", "github.com/golang/lint/golint"}
+	if out, err := exec.Command(comm[0], comm[1:]...).Output(); err != nil {
+		log.Fatal(err)
+	} else {
+		fmt.Print(string(out))
+	}
+}
+
+func goVet() {
+	comm := []string{"go", "vet", "./..."}
+	if out, err := exec.Command(comm[0], comm[1:]...).Output(); err != nil {
+		log.Fatal(err)
+	} else {
+		fmt.Print(string(out))
+	}
+}
+
+func goLint() {
+	comm := []string{"golint", "./..."}
+	if out, err := exec.Command(comm[0], comm[1:]...).Output(); err != nil {
+		log.Fatal(err)
+	} else if len(out) != 0 {
+		log.Fatal(string(out))
+	}
+}
+
+func goImports() {
+	comm := []string{"goimports", "-l", "./"}
+	if out, err := exec.Command(comm[0], comm[1:]...).Output(); err != nil {
+		log.Fatal(err)
+	} else if len(out) != 0 {
+		log.Fatal(string(out))
+	}
+}
+
+func main() {
+	action := ""
+	if len(os.Args) == 2 {
+		action = os.Args[1]
+	}
+
+	switch action {
+	case "build":
+		buildMami()
+	case "fmt":
+		fmtAll()
+	case "beforescript":
+		beforeScript()
+	case "govet":
+		goVet()
+	case "golint":
+		goLint()
+	case "goimports":
+		goImports()
+	case "script":
+		goVet()
+		goLint()
+		goImports()
+	default:
+		panic("Unknown action: " + action)
+	}
+}
